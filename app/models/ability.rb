e59393829aeb232e9c7433dcb4a5fd3beed6ca5f
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-07-14 08:50:42 -0500

    Actualiza y perite que director use tablas basicas y todo

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -27,6 +27,7 @@ class Ability  < Cor1440Gen::Ability
       case usuario.rol 
       when Ability::ROLSISTACT
         can :read, Cor1440Gen::Actividad
+        can :read, Cor1440Gen::Informe
         can :new, Cor1440Gen::Actividad
         can [:update, :create, :destroy], Cor1440Gen::Actividad, 
           oficina: { id: usuario.oficina_id}
@@ -35,21 +36,15 @@ class Ability  < Cor1440Gen::Ability
         can :new, Cor1440Gen::Actividad
         can [:update, :create, :destroy], Cor1440Gen::Actividad, 
           oficina: { id: usuario.oficina_id}
+        can [:new, :update, :create, :destroy], Cor1440Gen::Informe
         can :new, Usuario
         can [:read, :manage], Usuario, oficina: { id: usuario.oficina_id}
-      when Ability::ROLDIR
-        can [:read, :new, :update, :create, :destroy], Cor1440Gen::Actividad
-        can :manage, Usuario
-        can :manage, :tablasbasicas
-        @@tablasbasicas.each do |t|
-          c = Ability.tb_clase(t)
-          can :manage, c
-        end
       when Ability::ROLINV
-        cannot :buscar, Sip::Actividad
-        can :read, Sip::Actividad
-      when Ability::ROLADMIN
+        cannot :buscar, Cor1440Gen::Actividad
+        can :read, Cor1440Gen::Actividad
+      when Ability::ROLADMIN,Ability::ROLDIR
         can :manage, Cor1440Gen::Actividad
+        can :manage, Cor1440Gen::Informe
         can :manage, Sal7711Gen::Articulo
         can :manage, Usuario
         can :manage, :tablasbasicas
