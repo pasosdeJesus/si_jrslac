5ca6543391de4c55f09972a5451a18aa0d767533
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-07-24 20:27:25 -0500

    Actualiza

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,13 +2,29 @@
 
 class Ability  < Cor1440Gen::Ability
 
-  @@tablasbasicas += Sal7711Gen::Ability::BASICAS_NUEVAS + [
+  BASICAS_PROPIAS = [
     ['', 'poa']
   ]
+  @@tablasbasicas = Sip::Ability::BASICAS_PROPIAS + 
+    Cor1440Gen::Ability::BASICAS_PROPIAS +
+    Sal7711Gen::Ability::BASICAS_PROPIAS + 
+    BASICAS_PROPIAS 
 
-  @@basicas_seq_con_id += Sal7711Gen::Ability::BASICAS_SID_NUEVAS + [
-    ['', 'poa']
-  ]
+  BASICAS_ID_NOAUTO = []
+  @@basicas_id_noauto = Sip::Ability::BASICAS_ID_NOAUTO +
+    Cor1440Gen::Ability::BASICAS_ID_NOAUTO +
+    Sal7711Gen::Ability::BASICAS_ID_NOAUTO + BASICAS_ID_NOAUTO 
+  
+  NOBASICAS_INDSEQID = []
+  @@nobasicas_indice_seq_con_id = Sip::Ability::NOBASICAS_INDSEQID +
+    Cor1440Gen::Ability::NOBASICAS_INDSEQID +
+    Sal7711Gen::Ability::NOBASICAS_INDSEQID + NOBASICAS_INDSEQID
+
+  BASICAS_PRIO = []
+  @@tablasbasicas_prio = Sip::Ability::BASICAS_PRIO +
+    Cor1440Gen::Ability::Ability::BASICAS_PRIO +
+    Sal7711Gen::Ability::BASICAS_PRIO + BASICAS_PRIO
+
 
   # Ver documentacion de este metodo en app/models/ability de sip
   def initialize(usuario)
