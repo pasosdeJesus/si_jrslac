24cb5288de13f59895379078d20ddd267f1a7613
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-09-27 06:20:41 -0500

    aclarado control de acceso

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -36,6 +36,42 @@ class Ability  < Cor1440Gen::Ability
       Sal7711Gen::Ability::BASICAS_PRIO + BASICAS_PRIO
   end
 
+  ROLES = [
+      ["Administrador", ROLADMIN], 
+      ["Directivo", ROLDIR], 
+      ["Sistematizador de Actividades", ROLSISTACT]
+  ]
+
+  ROLES_CA = [
+    'Administrar actividades e informes. ' +
+    'Administrar convenios financiados. ' +
+    'Administrar artículos del archivo de prensa. ' +
+    'Administrar documentos en nube. ' +
+    'Administrar usuarios. ' + 
+    'Administrar respaldos. ' + 
+    'Administrar tablas básicas. ', #ROLADMIN, 1
+
+    '', # 2
+ 
+    'Administrar actividades e informes. ' +
+    'Administrar convenios financiados. ' +
+    'Administrar artículos del archivo de prensa. ' +
+    'Administrar documentos en nube. ' +
+    'Administrar usuarios. ' + 
+    'Administrar respaldos. ' + 
+    'Administrar tablas básicas. ', #ROLDIR, 3
+
+    '', # 4
+    '', # 5
+    '', # 6
+
+    'Administrar actividades. ' +
+    'Ver convenios financiados. ' +
+    'Ver artículos del archivo de prensa. ' +
+    'Ver documentos en nube. ' # ROLSISTACT
+
+  ]
+
   # Autorizaciones con CanCanCan
   def initialize(usuario = nil)
     # Sin autenticación puede consultarse información geográfica 
@@ -46,40 +82,24 @@ class Ability  < Cor1440Gen::Ability
     can :contar, Sip::Ubicacion
     can :buscar, Sip::Ubicacion
     can :lista, Sip::Ubicacion
-    can :descarga_anexo, Sip::Anexo
-    can :nuevo, Cor1440Gen::Actividad
-    can :nuevo, Sip::Ubicacion
     if !usuario.nil? && !usuario.rol.nil? then
+      can :descarga_anexo, Sip::Anexo
+      can :nuevo, Cor1440Gen::Actividad
+      can :nuevo, Sip::Ubicacion
       case usuario.rol 
       when Ability::ROLSISTACT
-        can :read, Cor1440Gen::Proyectofinanciero
-        can :read, Cor1440Gen::Actividad
-        can :read, Cor1440Gen::Informe
-        can :new, Cor1440Gen::Actividad
-        can [:update, :create, :destroy], Cor1440Gen::Actividad, 
-          oficina: { id: usuario.oficina_id}
-        can :read, Heb412Gen::Doc
-      when Ability::ROLCOOR
-        can :read, Cor1440Gen::Proyectofinanciero
-        can :read, Cor1440Gen::Actividad
-        can :new, Cor1440Gen::Actividad
-        can [:update, :create, :destroy], Cor1440Gen::Actividad, 
-          oficina: { id: usuario.oficina_id}
-        can [:new, :update, :create, :destroy], Cor1440Gen::Informe
-        can :new, Usuario
-        can [:read, :manage], Usuario, oficina: { id: usuario.oficina_id}
-        can :read, Heb412Gen::Doc
-      when Ability::ROLINV
-        cannot :buscar, Cor1440Gen::Actividad
-        can :read, Cor1440Gen::Actividad
-      when Ability::ROLADMIN,Ability::ROLDIR
-        can :manage, Cor1440Gen::Proyectofinanciero
         can :manage, Cor1440Gen::Actividad
         can :manage, Cor1440Gen::Informe
-        can :manage, Sal7711Gen::Articulo
+        can :read, Cor1440Gen::Proyectofinanciero
+        can :read, Heb412Gen::Doc
+      when Ability::ROLADMIN,Ability::ROLDIR
         can :manage, ::Usuario
-        can :manage, Sip::Respaldo7z
+        can :manage, Cor1440Gen::Actividad
+        can :manage, Cor1440Gen::Informe
+        can :manage, Cor1440Gen::Proyectofinanciero
         can :manage, Heb412Gen::Doc
+        can :manage, Sal7711Gen::Articulo
+        can :manage, Sip::Respaldo7z
         can :manage, :tablasbasicas
         tablasbasicas.each do |t|
           c = Ability.tb_clase(t)
