73ad7188496c5ba62ff48b46ffcfb704275c636d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-05-03 05:29:54 -0500

    rangoedad

--- db/datos-basicos.sql
+++ db/datos-basicos.sql
@@ -7,3 +7,10 @@ INSERT INTO public.heb412_gen_campoplantillahcm (id, plantillahcm_id, nombrecamp
 INSERT INTO public.heb412_gen_campoplantillahcm (id, plantillahcm_id, nombrecampo, columna) VALUES (55, 17, 'statusmigratorio', 'BW');
 INSERT INTO public.heb412_gen_campoplantillahcm (id, plantillahcm_id, nombrecampo, columna) VALUES (56, 17, 'victimas', 'AH');
 
+
+INSERT INTO public.sivel2_gen_rangoedad (id, nombre, rango, limiteinferior, limitesuperior, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (101, '0 A 5', NULL, 0, 5, '2018-12-21', NULL, '2018-12-21 15:07:39.566166', '2018-12-21 15:07:39.566166', '');
+INSERT INTO public.sivel2_gen_rangoedad (id, nombre, rango, limiteinferior, limitesuperior, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (102, '6 A 12', NULL, 6, 12, '2018-12-21', NULL, '2018-12-21 15:07:51.096465', '2018-12-21 15:07:51.096465', '');
+INSERT INTO public.sivel2_gen_rangoedad (id, nombre, rango, limiteinferior, limitesuperior, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (103, '13 A 17', NULL, 13, 17, '2018-12-21', NULL, '2018-12-21 15:07:57.388678', '2018-12-21 15:07:57.388678', '');
+INSERT INTO public.sivel2_gen_rangoedad (id, nombre, rango, limiteinferior, limitesuperior, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (104, '18 A 26', NULL, 18, 26, '2018-12-21', NULL, '2018-12-21 15:08:04.203403', '2018-12-21 15:08:04.203403', '');
+INSERT INTO public.sivel2_gen_rangoedad (id, nombre, rango, limiteinferior, limitesuperior, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (105, '27 A 59', NULL, 27, 59, '2018-12-21', NULL, '2018-12-21 15:08:17.420757', '2018-12-21 15:08:17.420757', '');
+INSERT INTO public.sivel2_gen_rangoedad (id, nombre, rango, limiteinferior, limitesuperior, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (107, 'DE 60 EN ADELANTE', NULL, 60, 2000, '2018-12-21', NULL, '2018-12-21 15:08:58.334547', '2018-12-21 15:08:58.334547', '');
